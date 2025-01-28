7d0b65da95546c0ad7ecc1700e309900a3060e93
Jaehyun Kim <68116156+kdjames0930@users.noreply.github.com>
2025-01-28 02:24:38 +0000

    basics commit

--- mvp.py
+++ mvp.py
@@ -5,6 +5,60 @@ load_dotenv()
 # 1. 업비트 차트 데이터 가져오기 (30일치 일봉)
 import pyupbit
 df = pyupbit.get_ohlcv("KRW-BTC", count=30, interval="day")
-print(df.to_json())
 
 # 2. AI에게 데이터 제공하고 판단 받는다.
+import json
+from openai import OpenAI
+client = OpenAI(api_key=os.getenv("DEEPSEEK_API_KEY"), base_url="https://api.deepseek.com")
+
+system_prompt = """
+You are an expert in Bitcoin investing. Tell me whether to buy, sell or hold at the moment based on the chart data provided. Response in json format.
+
+EXAMPLE INPUT: bitcoin chart data
+
+EXAMPLE JSON OUTPUT:
+{
+    "decision": "sell",
+    "reason": "some technical reason"
+}
+"""
+
+user_prompt = df.to_json()
+
+messages = [{"role": "system", "content": system_prompt},
+            {"role": "user", "content": user_prompt}]
+
+response = client.chat.completions.create(
+    model="deepseek-chat",
+    messages=messages,
+    response_format={
+        'type': 'json_object'
+    }
+)
+
+# AI 판단에 따라 매수/매도 진행
+
+result = json.loads(response.choices[0].message.content)
+print(result)
+print(result['decision'])
+
+access = os.getenv("UPBIT_ACCESS_KEY")
+secret = os.getenv("UPBIT_SECRET_KEY")
+upbit = pyupbit.Upbit(access, secret)
+
+# Buy
+if result["decision"] == "buy":
+    my_krw = upbit.get_balance("KRW")
+    if my_krw*0.9995 > 5000 :
+        print(upbit.buy_market_order("KRW-BTC", my_krw*0.9995))
+        print("buy, ", result['reason'])
+# Sell
+if result["decision"] == "sell":
+    my_btc = upbit.get_balance("KRW-BTC")
+    current_price = pyupbit.get_orderbook(ticker="KRW-BTC")['orderbook_units'][0]['ask_price']
+    if my_btc * current_price > 5000:
+        print(upbit.sell_market_order("KRW-BTC", ))
+        print("sell, ", result['reason'])
+# Hold
+if result["decision"] == "hold":
+    print("hold, ", result['reason'])
--- test.py
+++ test.py
@@ -4,4 +4,4 @@ load_dotenv()
 
 print(os.getenv("UPBIT_ACCESS_KEY"))
 print(os.getenv("UPBIT_SECRET_KEY"))
-print(os.getenv("OPENAI_API_KEY"))
+print(os.getenv("DEEPSEEK_API_KEY"))
